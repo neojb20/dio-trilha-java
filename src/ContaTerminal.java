9e8eca3a5cf3571bb70592718fc1bf4239ece54a
Nilson Silva <neojb20@gmail.com>
2023-08-23 00:45:34 -0300

    commit para corrigir os erros da pasta main.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // classe scanner para inserir os dados na aplicacão.
+        Scanner scanner = new Scanner(System.in);
+
+        contaCliente contaCliente = new contaCliente();
+
+        // pedindo informacoes paraentrada o usuario.
+        System.out.println("Olá por favor, Dígite o número de sua agência!.");
+        contaCliente.numero = scanner.nextInt();
+        System.out.println("Dígite o numero de sua agencia!");
+        contaCliente.agencia = scanner.next();
+        System.out.println("Dígite seu Nome!");
+        contaCliente.nomeCliente = scanner.next();
+        System.out.println("Dígite o valor inicial que pretende depositar");
+        contaCliente.saldo = scanner.nextDouble();
+
+        // exibicao de menssagem final para o1 usuario.
+        System.out.println("olá, sr.a. " + contaCliente.nomeCliente
+                + " obrigado por criar uma conta em nosso banco, sua agencia é " + contaCliente.agencia + ", conta "
+                + contaCliente.numero + " seu saldo " + contaCliente.saldo + " já está disponivel para saque");
+
     }
 }
---
+++ src/contaCliente.java
@@ -0,0 +1,24 @@
+public class contaCliente {
+
+    int numero = 0;
+    String agencia;
+    String nomeCliente;
+    double saldo = 0.0d;
+
+    public void numero() {
+        numero = numero;
+    }
+
+    public void agencia() {
+        agencia = agencia;
+    }
+
+    public void nomeCliente() {
+        nomeCliente = nomeCliente;
+    }
+
+    public void saldo() {
+        saldo = saldo;
+    }
+
+}
